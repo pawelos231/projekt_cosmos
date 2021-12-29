ac71357cfc882040b55f56acbf1e2d0b6db52da5
pawelLinek <91089116+pawelLinek@users.noreply.github.com>
2021-12-29 15:10:34 +0100

    zestam sied

--- pages/api/form.js
+++ pages/api/form.js
@@ -3,30 +3,12 @@ const prisma = new PrismaClient();
 export default function HandleForm(req, res) {
   if (req.method === "POST") {
     const body = req.body;
-    const parsedobj = JSON.parse(body);
-
-    const FindIfPostExists = async () => {
-      const Database = await prisma.contact.findFirst({
-        where: {
-          email: parsedobj.email,
-        },
-      });
-      if (Database !== null) {
-        return false;
-      } else {
-        return true;
-      }
-    };
 
     const bruh = async () => {
-      if (await FindIfPostExists()) {
-        const post = await prisma.contact.create({
-          data: JSON.parse(body),
-        });
-        console.log(post);
-      } else {
-        console.log("podany email jest juz w bazie");
-      }
+      const post = await prisma.contact.create({
+        data: JSON.parse(body),
+      });
+      console.log(post);
     };
     res.status(200).json(JSON.parse(body));
     bruh();
