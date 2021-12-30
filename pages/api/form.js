65f484ac2fa08a331ee2d47d7da0e8662cefd1bc
pawelLinek <91089116+pawelLinek@users.noreply.github.com>
2021-12-30 13:48:52 +0100

    obyyYyyy

--- pages/api/form.js
+++ pages/api/form.js
@@ -8,28 +8,30 @@ export default async function HandleForm(req, res) {
     const parsedobj = JSON.parse(body);
     console.log(parsedobj);
     const SendMail = async () => {
-      let transporter = await nodemailer.createTransport({
-        service: "hotmail",
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-          user: `pawelosssek@hotmail.com`, // generated ethereal user
-          pass: `JebacPolicje123`, // generated ethereal password
-        },
-      });
-      let mailOptions = {
-        from: `pawelosssek@hotmail.com`,
-        to: `pawellinek3d@gmail.com`,
-        subject: `Hi my name is ${parsedobj.firstName}`,
-        text: `Hello my name is ${parsedobj.firstName} and i would like to write this massage to you: ${parsedobj.message}, here is my conatact ${parsedobj.email} `,
-      };
-      await transporter.sendMail(mailOptions, function (err, data) {
-        if (err) {
-          console.log("error oh my god", err);
-        } else {
-          console.log("EMAIL SENT!");
-        }
-      });
+      for (let i = 0; i < 3; i++) {
+        let transporter = await nodemailer.createTransport({
+          service: "hotmail",
+          port: 587,
+          secure: false, // true for 465, false for other ports
+          auth: {
+            user: `pawelosssek@hotmail.com`, // generated ethereal user
+            pass: `JebacPolicje123`, // generated ethereal password
+          },
+        });
+        let mailOptions = {
+          from: `pawelosssek@hotmail.com`,
+          to: `pawellinek3d@gmail.com`,
+          subject: `Hi my name is ${parsedobj.firstName}`,
+          text: `Hello my name is ${parsedobj.firstName} and i would like to write this massage to you: ${parsedobj.message}, here is my conatact ${parsedobj.email} `,
+        };
+        await transporter.sendMail(mailOptions, function (err, data) {
+          if (err) {
+            console.log("error oh my god", err);
+          } else {
+            console.log("EMAIL SENT!");
+          }
+        });
+      }
     };
     const bruh = async () => {
       const post = await prisma.contact.create({
