8b4d7ae2f2f5d9eaf923e9a69efd46bb9720adc6
pawelLinek <91089116+pawelLinek@users.noreply.github.com>
2021-12-31 01:33:46 +0100

    obyyYyyyyedd

--- pages/api/form.js
+++ pages/api/form.js
@@ -8,35 +8,39 @@ export default async function HandleForm(req, res) {
     const parsedobj = JSON.parse(body);
     console.log(parsedobj);
     const SendMail = async () => {
+      function waitforme(ms) {
+        return new Promise((resolve) => {
+          setTimeout(resolve, ms);
+        });
+      }
       for (let i = 0; i < 4; i++) {
-        setTimeout(() => {
-          let transporter = nodemailer.createTransport({
-            service: "hotmail",
-            port: 465,
-            secure: false,
-            tls: {
-              ciphers: "SSLv3",
-            }, // true for 465, false for other ports
-            auth: {
-              user: `pawelosssek@hotmail.com`, // generated ethereal user
-              pass: `JebacPolicje123`, // generated ethereal password
-            },
-          });
-          let mailOptions = {
-            from: `pawelosssek@hotmail.com`,
-            to: `pawellinek3d@gmail.com`,
-            subject: `Hi my name is ${parsedobj.firstName}`,
-            text: `Hello my name is ${parsedobj.firstName} and i would like to write this massage to you: ${parsedobj.message}, here is my conatact ${parsedobj.email} `,
-          };
-          transporter.sendMail(mailOptions, function (err, data) {
-            if (err) {
-              console.log("error oh my god", err);
-            } else {
-              console.log("EMAIL SENT!");
-              flag = true;
-            }
-          });
-        }, 1000);
+        let transporter = nodemailer.createTransport({
+          service: "hotmail",
+          port: 465,
+          secure: false,
+          tls: {
+            ciphers: "SSLv3",
+          }, // true for 465, false for other ports
+          auth: {
+            user: `pawelosssek@hotmail.com`, // generated ethereal user
+            pass: `JebacPolicje123`, // generated ethereal password
+          },
+        });
+        let mailOptions = {
+          from: `pawelosssek@hotmail.com`,
+          to: `pawellinek3d@gmail.com`,
+          subject: `Hi my name is ${parsedobj.firstName}`,
+          text: `Hello my name is ${parsedobj.firstName} and i would like to write this massage to you: ${parsedobj.message}, here is my conatact ${parsedobj.email} `,
+        };
+        transporter.sendMail(mailOptions, function (err, data) {
+          if (err) {
+            console.log("error oh my god", err);
+          } else {
+            console.log("EMAIL SENT!");
+            flag = true;
+          }
+        });
+        await waitforme(1000);
       }
     };
     const bruh = async () => {
