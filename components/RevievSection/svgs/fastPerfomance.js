58d9a510bc14240dfd21b09214c7dd92d5365b8f
pawelLinek <91089116+pawelLinek@users.noreply.github.com>
2022-01-18 10:34:46 +0100

    fixw

--- components/RevievSection/svgs/fastPerfomance.js
+++ components/RevievSection/svgs/fastPerfomance.js
@@ -7,7 +7,6 @@ const FastPerfomance = () => {
       focusable="false"
       data-prefix="fas"
       data-icon="moon"
-      class="svg-inline--fa fa-moon fa-w-16"
       role="img"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 512 512"
